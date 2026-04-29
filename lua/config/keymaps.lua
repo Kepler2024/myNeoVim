b41be2db79ceb5baecd9d2e2ec4e2e7dffad7b6e
Harry C. Kepler <qilin2603379191@gmail.com>
2026-04-29 10:42:02 +0200

    keymaps change and dashboard

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,16 +1,28 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("n", "<leader>bd", function()
-  Snacks.bufdelete()
-  if #vim.fn.getbufinfo({ buflisted = 1 }) == 0 then
-    Snacks.dashboard()
-  end
-end, { desc = "Delete buffer and show dashboard" })
+
+-- vim.keymap.set("n", "<leader>bd", function()
+--   Snacks.bufdelete()
+--   if #vim.fn.getbufinfo({ buflisted = 1 }) == 0 then
+--     Snacks.dashboard()
+--   end
+-- end, { desc = "Delete buffer and show dashboard" })
 
 -- 回到 dashboard
 vim.keymap.set("n", "<leader>;", function()
-  if pcall(require, "snacks") then
-    Snacks.dashboard()
+  -- 保存所有有文件名且已修改的 buffer
+  vim.cmd("silent! wall")
+  -- 关闭所有 buffer
+  vim.cmd("silent! %bdelete!")
+  Snacks.dashboard()
+end, { desc = "Save all, close all buffers, open Dashboard" })
+
+-- 删除 buffer，但保留窗口
+vim.keymap.set("n", "<leader>bd", function()
+  local bufs = vim.fn.getbufinfo({ buflisted = 1 })
+  if #bufs > 1 then
+    vim.cmd("BufferLineCycleNext")
   end
-end, { desc = "Dashboard" })
+  Snacks.bufdelete()
+end, { desc = "Delete buffer (keep window)" })
