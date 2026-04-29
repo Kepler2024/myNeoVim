b091511550eaef8e7465c78e2d66085e32d7ec33
Harry C. Kepler <qilin2603379191@gmail.com>
2026-04-29 12:10:30 +0200

    feat: auto explorer

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,17 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 1 then
+      local arg = vim.fn.argv()[1]
+      if vim.fn.isdirectory(arg) == 0 then
+        Snacks.explorer()
+        vim.schedule(function()
+          vim.cmd("wincmd p")
+        end)
+      end
+    end
+  end,
+})
