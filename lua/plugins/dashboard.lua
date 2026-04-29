bf92b09eee5be86129bf71db00a86b1783741f3e
Harry C. Kepler <qilin2603379191@gmail.com>
2026-04-29 11:58:21 +0200

    style: add icons

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -18,7 +18,7 @@ return {
         keys = {
           { icon = "\u{f422} ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
           { icon = "\u{f07b} ", key = "p", desc = "Projects", action = ":lua Snacks.dashboard.pick('projects')" },
-          { icon = "\u{f4a1} ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+          { icon = "\u{f70f} ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = "\u{f0c5} ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
           {
             icon = "\u{f013} ",
@@ -38,7 +38,7 @@ return {
         { section = "keys", gap = 1, padding = 1 },
         -- { section = "startup" }, -- 那个 "Neovim loaded..." 行
         {
-          text = { { "  KK@terminal · harrykepler.com", hl = "footer" } },
+          text = { { "\u{f06d}  KK@terminal · harrykepler.com", hl = "footer" } },
           align = "center",
         },
       },
