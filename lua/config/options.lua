5d4871f9d73f5d3a882eb9c304add6c071ca0707
Harry C. Kepler <qilin2603379191@gmail.com>
2026-04-30 20:40:01 +0200

    feat: change indent length

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.shiftwidth = 4 -- 自动缩进的空格数
+vim.opt.tabstop = 4 -- Tab 显示为几个空格
+vim.opt.softtabstop = 4 -- 编辑时 Tab/Backspace 的步长
+vim.opt.expandtab = true -- Tab 转空格
