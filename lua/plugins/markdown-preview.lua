296ff7e4edff00ce0da056be39e3919094fb41b9
Harry C. Kepler <qilin2603379191@gmail.com>
2026-05-09 21:33:23 +0200

    feat!: vscode experience markdwon preview

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -1,7 +1,18 @@
 return {
-  "MeanderingProgrammer/render-markdown.nvim",
-  dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-mini/mini.nvim" },
-  ---@module 'render-markdown'
-  ---@type render.md.UserConfig
-  opts = {},
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  build = "cd app && npm install",
+  init = function()
+    vim.g.mkdp_filetypes = { "markdown" }
+    vim.g.mkdp_port = "8089"
+    vim.g.mkdp_auto_close = 1
+    vim.g.mkdp_browserfunc = "OpenInIterm"
+    vim.cmd([[
+      function! OpenInIterm(url)
+        let html = '<!DOCTYPE html><meta http-equiv="refresh" content="0; url=' . a:url . '">'
+        call writefile([html], expand('~/iterm-mdpreview.html'))
+      endfunction
+    ]])
+  end,
+  ft = { "markdown" },
 }
